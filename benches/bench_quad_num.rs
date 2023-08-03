7890f053ff78d571e8888d30b7f404f80b0ff7ec
Colby Austin Brown <colbyaustinbrown@gmail.com>
2023-08-03 10:48:57 -0700

    Fix quad nums benchmark

--- benches/bench_quad_num.rs
+++ benches/bench_quad_num.rs
@@ -1,25 +1,22 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use std::rc::Rc;
-
-use libbgs::factorization::*;
-use libbgs::quad_field::*;
-use libbgs::semigroup::*;
+use libbgs::numbers::factorization::*;
+use libbgs::numbers::quad_field::*;
 
 fn find_sqrt(n: u128) {
-    let fp = Rc::new(QuadFieldExt::make(
-        &Rc::new(Factorization::new(
+    let fp = QuadField::make(
+        Factorization::new(
             vec![(2, 1), (7, 1), (13, 1), (29, 2), (43, 1), (705737, 1), (215288719, 1)]
-        )),
+        ),
         Factorization::new(
             vec![(2, 4), (3, 1), (5, 2), (11, 2), (17, 1), (19, 1), (23, 1), (97, 1), (757, 1), (1453, 1), (8689, 1)]
         )
-    ));
+    );
     for i in 3..103 {
         let mut x = fp.int_sqrt(i);
         let y = x.clone();
         assert_ne!(x, i);
-        x.multiply(&y);
+        x.multiply(&y, &fp);
         assert_eq!(x, i);
     }
 }
