3b49e2f281e062faa8fe9cb4e91310b0f94ca4a0
Colby Austin Brown <colbyaustinbrown@gmail.com>
2023-08-07 18:16:46 -0700

    Update the sylow stream bencher

--- benches/bench_sylow_stream.rs
+++ benches/bench_sylow_stream.rs
@@ -1,22 +1,24 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use libbgs::numbers::sylow_factory::*;
+use libbgs::numbers::sylow_stream::*;
 use libbgs::numbers::sylow::*;
 use libbgs::numbers::factorization::*;
 
-fn run_factory() {
+fn run_stream() {
         let fp = Factorization::new(vec![(2, 1), (3, 3), (5, 3), (757, 1)]);
         let g = SylowDecomp::new(&fp);
 
-        let factory = SylowFactory::new(&g, vec![0, 3, 2, 1], flags::NONE); 
+        let stream = SylowStreamBuilder::new(&g)
+            .add_target(vec![0, 3, 2, 1]) 
+            .build();
 
         let mut count = 0;
-        for y in factory { count += 1; }
+        for y in stream { count += 1; }
         assert_eq!(count, 272160);
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("run factory", |b| b.iter(|| run_factory()));
+    c.bench_function("run stream", |b| b.iter(|| run_stream()));
 }
 
 criterion_group!(benches, criterion_benchmark);
