54c59b2724548900829f8b08e40a2010891af40c
Colby Austin Brown <colbyaustinbrown@gmail.com>
2023-08-22 06:47:33 -0700

    Move main to samples folder
    close #9

--- samples/run_orbit_tester.rs
+++ samples/run_orbit_tester.rs
@@ -1,67 +1,14 @@
 use libbgs::markoff::coord::*;
+use libbgs::markoff::orbit_tester::*;
 use libbgs::numbers::factorization::*;
 use libbgs::numbers::fp::*;
 use libbgs::numbers::quad_field::*;
 use libbgs::numbers::sylow::*;
 use libbgs::numbers::sylow_stream::*;
-// use libbgs::numbers::group::*;
-// use libbgs::markoff::triple::*;
-use libbgs::markoff::orbit_tester::*;
 
 const BIG_P: u128 = 1_000_000_000_000_000_124_399;
 
 fn main() {
-    /*
-    let p = 13;
-    let fp = Factorization::new(vec![(2, 2), (3, 1)]);
-    let fp2 = QuadField::make(
-        fp,
-        Factorization::new(vec![(2, 1), (7, 1)])
-    );
-    for a in 0..p {
-        for b in 0..p {
-            for c in 0..p {
-                if (a*a + b*b + c*c + 3*a*b*c*p - 3*a*b*c) % p != 0 { continue; }
-                let x = MarkoffTriple::make(a, b, c, &fp2);
-                for m in x.rot(Pos::A, &fp2) {
-                    println!("{} {} {} ({})", m.a.v(), m.b.v(), m.c.v(), m.ord(Pos::C, &fp2));
-                }
-                println!();
-            }
-        }
-    }
-    */
-    /*
-    let fp = Factorization::new(vec![(2, 1), (7, 1), (13, 1), (29, 2), (43, 1), (705737, 1), (215288719, 1)]);
-    let fp2 = QuadField::make(
-        fp.clone(),
-        Factorization::new(vec![(2, 4), (3, 1), (5, 2), (11, 2), (17, 1), (19, 1), (23, 1), (97, 1), (757, 1), (1453, 1), (8689, 1)])
-    );
-    println!("p is {}", fp2.p());
-    let decomp = SylowDecomp::new(&fp);
-    //let stream = sylow_stream(&decomp, &vec![1, 1, 1, 0, 0, 0, 0], Mode::LEQ);
-    let mut count = 0;
-    let builder = SylowStreamBuilder::new(&decomp)
-        .add_target(vec![0, 1, 1, 0, 0, 0, 0])
-        .add_target(vec![0, 0, 1, 1, 0, 0, 0])
-        // .add_flag(flags::NO_UPPER_HALF)
-        .add_flag(flags::LEQ);
-
-    let tester_builder = OrbitTester::new(&fp);
-    for x in builder.build() {
-
-    }
-    for y in builder.build() {
-        let mut yin = y.clone();
-        // println!("{:?}", y.coords);
-        yin.invert(&decomp);
-        let a = Coord::from_chi(Right((y.to_product(&decomp), yin.to_product(&decomp))), &fp2);
-        println!("{} has order {}", a.v(), a.get_ord(&fp2).value());
-        count += 1;
-    }
-    println!("total: {count}");
-    */
-
     let fp = Factorization::new(vec![
         (2, 1),
         (7, 1),
@@ -140,9 +87,7 @@ fn main() {
         }
 
         for (key, ord) in orbits {
-            println!(
-                "For coordinate {x}: Representative: {key} is an orbit of order {ord}."
-            );
+            println!("For coordinate {x}: Representative: {key} is an orbit of order {ord}.");
         }
     }
     println!("{repless_count} coordinates had no representative.");
