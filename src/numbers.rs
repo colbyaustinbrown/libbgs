116d7dacd644d6681c2353170c645ddd33ffc853
Colby Austin Brown <colbyaustinbrown@gmail.com>
2023-08-28 11:10:54 +0200

    Add description for the numbers module

--- src/numbers.rs
+++ src/numbers.rs
@@ -1,3 +1,8 @@
+//! Types and utilities for manipulating numbers in various types of finite fields.
+//!
+//! This module contains modular integers (i.e., $\mathbb{Z} / p\mathbb{Z}$ for prime $p$),
+//! their quadratic finite field extensions (i.e., $\mathbb{Z} / p^2\mathbb{Z}$ for prime $p$), and
+//! decompositions into direct sums of Sylow subgroups.
 pub mod factor_stream;
 pub mod factorization;
 pub mod fp;
