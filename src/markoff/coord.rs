2c111cd4d7bfbf4658e528c6713441a0b9b7c0c1
Colby Austin Brown <colbyaustinbrown@gmail.com>
2023-09-18 17:29:14 -0500

    turn Coord into a type alias for FpNum<P>

--- src/markoff/coord.rs
+++ src/markoff/coord.rs
@@ -6,8 +6,8 @@ use crate::util::*;
 /// A coordinate for a Markoff triple.
 /// May represent any of $a$, $b$, or $c$ in a Markoff triple $(a, b, c)$.
 /// This is a single field struct containing only an `FpNum<P>` for prime `P`.
-#[derive(PartialEq, Clone, Copy, Eq, Debug)]
-pub struct Coord<const P: u128>(FpNum<P>);
+// pub struct Coord<const P: u128>(FpNum<P>);
+pub type Coord<const P: u128> = FpNum<P>;
 
 impl<const P: u128> Coord<P> {
     /// Returns an element $\chi$ such that, for a coordinate $a$, $a = \chi + \chi^{-1}$.
@@ -15,7 +15,7 @@ impl<const P: u128> Coord<P> {
     /// will be a `Right<FpNum<P>>`. Otherwise, $\chi \in \mathbb{F}\_{p^2}$, and the result will
     /// be a `Left<QuadNum<P>>`.
     pub fn to_chi(&self) -> Either<QuadNum<P>, FpNum<P>> {
-        let v3 = long_multiply(self.0 .0, 3, P);
+        let v3 = long_multiply(self.0, 3, P);
         let disc = intpow(v3, 2, P);
         let disc = (disc + P - 4) % P;
         QuadNum::int_sqrt_either(disc).map_either(
@@ -55,7 +55,7 @@ impl<const P: u128> Coord<P> {
 
         // We use the non-normalized equation:
         // x^2 + y^2 + z^2 - xyz = 0
-        Coord(chi + chi_inv)
+        chi + chi_inv
     }
 
     /// Returns the coordinate $a = \chi + \chi^{-1}$, where $\chi \in \mathbb{F}_{p^2}$.
@@ -71,14 +71,14 @@ impl<const P: u128> Coord<P> {
 
         // We use the non-normalized equation:
         // x^2 + y^2 + z^2 - xyz = 0
-        Coord(FpNum::from((chi + chi_inv).0))
+        FpNum::from((chi + chi_inv).0)
     }
 
     /// Returns an iterator yielding the coordinates $(b, c)$ contained in the orbit with fixed coordinate
     /// $a$ (the coordinate on which `rot` is called), beginning with $(a, b, c)$.
     pub fn rot(self, b: Coord<P>, c: Coord<P>) -> impl Iterator<Item = (Coord<P>, Coord<P>)> {
         std::iter::successors(Some((b, c)), move |(y, z)| {
-            let (b_, c_) = (*z, Coord::from(self.0 * z.0 + P.into() - y.0));
+            let (b_, c_) = (*z, FpNum::from(self.0 * z.0 + P - y.0));
             if b_ == b && c_ == c {
                 None
             } else {
@@ -101,35 +101,5 @@ impl<const P: u128> Coord<P> {
         self.to_chi()
             .as_ref()
             .either(|l| l.order().value(), |r| r.order().value())
-    }
-}
-
-impl<const P: u128> From<u128> for Coord<P> {
-    fn from(value: u128) -> Coord<P> {
-        Coord(FpNum::from(value))
-    }
-}
-
-impl<const P: u128> From<FpNum<P>> for Coord<P> {
-    fn from(value: FpNum<P>) -> Coord<P> {
-        Coord(value)
-    }
-}
-
-impl<const P: u128> From<Coord<P>> for u128 {
-    fn from(value: Coord<P>) -> u128 {
-        u128::from(value.0)
-    }
-}
-
-impl<const P: u128> From<Coord<P>> for FpNum<P> {
-    fn from(value: Coord<P>) -> FpNum<P> {
-        value.0
-    }
-}
-
-impl<const P: u128> std::fmt::Display for Coord<P> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        self.0.fmt(f)
     }
 }
