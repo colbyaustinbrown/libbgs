9f0c5decdad2cce0a8e6fa8164bd2ec9ff9a955f
Colby Austin Brown <colbyaustinbrown@gmail.com>
2023-11-21 16:38:39 -0600

    Add unused_tuple_struct_field lint

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![feature(array_methods)]
-#![warn(missing_docs, clippy::all)]
-#![allow(dead_code, clippy::new_without_default, clippy::needless_range_loop)]
+#![warn(missing_docs, unused_crate_dependencies, unused_lifetimes, unused_tuple_struct_fields, variant_size_differences, clippy::all)]
+#![allow(clippy::new_without_default, clippy::needless_range_loop)]
 
 //! A library for the manipulation and investigation of Markoff numbers and Markoff graphs modulo
 //! primes, specifically with a focus on the Bourgain, Gamburd, and Sarnak algorithm.
